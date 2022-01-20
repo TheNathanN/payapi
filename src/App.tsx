ac73c1b1f620bc9f87b25ecb1f2a23b58f21c26d
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-01-19 19:17:58 -0600

    Update root html and set up React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
+import { Routes, Route, Link } from 'react-router-dom';
+// Pages
+import Home from './pages/Home';
+import Pricing from './pages/Pricing';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const App = () => {
   return (
     <div className=''>
       <h1>Hello</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
