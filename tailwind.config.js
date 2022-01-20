9aa9eb69c0ec6c8f4c98b1ed7d123fcd9904baea
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2022-01-19 18:42:26 -0600

    first commit w/ tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      primPink: '#ba4270',
+      primWhite: '#fbfcfe',
+      secBlue: '#36536b',
+      secDarkBlue: '#1b262f',
+      secPink: '#da6d97',
+      secLightBlue: '#6c8294',
+    },
+    fontFamily: {
+      serif: ['DM Serif Display', 'serif'],
+      sans: ['Public Sans', 'sans-serif'],
+    },
+    fontSize: {
+      h1lg: ['72px', '72px'],
+      h1sm: ['56px', '56px'],
+      h2: ['48px', '56px'],
+      h3lg: ['32px', '40px'],
+      h3sm: ['18px', '25px'],
+      h4: ['24px', '32px'],
+      body: ['15px', '28px'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
