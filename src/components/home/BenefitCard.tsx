a2831306c7719e12b89726fb0b3cde58e6b6c138
Nathan Nicholson <thenathannicholson@gmail.com>
2022-01-30 18:14:52 -0600

    Style the BenefitCard for the different direction props

--- src/components/home/BenefitCard.tsx
+++ src/components/home/BenefitCard.tsx
@@ -1,20 +1,46 @@
 import { CardInfo } from '../../types';
 
-const BenefitCard = ({ title, info, image, imgAlt, imgStyling }: CardInfo) => {
+const BenefitCard = ({ title, info, image, imgAlt, imgStyling, direction }: CardInfo) => {
   return (
-    <div className='flex flex-col items-center justify-center '>
-      <div className='flex justify-center items-center max-w-md '>
-        <img src={image} alt={imgAlt} className={imgStyling} />
-      </div>
+    <div>
+      {/* The direction prop is for determing which div to show with the desktop display */}
+      {direction === 'left' ? (
+        <div className='flex flex-col items-center justify-center lg:flex-row '>
+          <div className='flex justify-center items-center max-w-md '>
+            <img src={image} alt={imgAlt} className={imgStyling} />
+          </div>
 
-      <div className='text-center px-4 '>
-        <h2 className='font-serif text-h3lg text-secBlue md:text-h2 '>
-          {title}
-        </h2>
-        <p className='font-sans text-body text-secLightBlue my-4 max-w-lg '>
-          {info}
-        </p>
-      </div>
+          <div className='text-center px-4 '>
+            <h2 className='font-serif text-h3lg text-secBlue md:text-h2 '>
+              {title}
+            </h2>
+            <p className='font-sans text-body text-secLightBlue my-4 max-w-lg '>
+              {info}
+            </p>
+          </div>
+        </div>
+      ) : ''}
+
+      {direction === 'right' ? (
+        <div className='flex flex-col items-center justify-center lg:flex-row '>
+          <div className='flex justify-center items-center max-w-md lg:hidden '>
+            <img src={image} alt={imgAlt} className={imgStyling} />
+          </div>
+
+          <div className='text-center px-4 '>
+            <h2 className='font-serif text-h3lg text-secBlue md:text-h2 '>
+              {title}
+            </h2>
+            <p className='font-sans text-body text-secLightBlue my-4 max-w-lg '>
+              {info}
+            </p>
+          </div>
+
+          <div className='hidden lg:flex justify-center items-center max-w-md '>
+            <img src={image} alt={imgAlt} className={imgStyling} />
+          </div>
+        </div>
+      ) : ''}
     </div>
   );
 };
--- src/components/home/Benefits.tsx
+++ src/components/home/Benefits.tsx
@@ -10,6 +10,7 @@ const Benefits = () => {
         image={benefits[0].image}
         imgAlt={benefits[0].imgAlt}
         imgStyling={benefits[0].imgStyling}
+        direction='left'
       />
 
       <BenefitCard
@@ -18,6 +19,7 @@ const Benefits = () => {
         image={benefits[1].image}
         imgAlt={benefits[1].imgAlt}
         imgStyling={benefits[1].imgStyling}
+        direction='right'
       />
     </section>
   );
